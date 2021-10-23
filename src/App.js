53f92eb142da73219ea9b57dd9ef835247518f1d
Ali Omar <ali95.muhom@gmail.com>
2021-10-23 22:11:13 +0300

    Start the Project

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import { CardList } from "./compononets/card-list/card.list";
+// import { Searchbox } from "./compononets/SearchField/SearchBox";
+import react from "react";
+import { Searchbox } from "./compononets/SearchField/SearchBox";
+
+class App extends react.Component {
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+            searchField: "",
+        };
+    }
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((users) => this.setState({ monsters: users }));
+    }
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value });
+    };
+    render() {
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase()),
+        );
+
+        return (
+            <div className="App">
+                <h1>Monsters Rolodex</h1>
+                <Searchbox
+                    placeholder="Search Monsters"
+                    handleChange={this.handleChange}
+                />
+                <CardList monsters={filteredMonsters} />
+            </div>
+        );
+    }
+}
+
+export default App;
